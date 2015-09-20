ba84a0b52cc92f8d20b1a8bcd2ac4da9707f1ef7
poldy79 <leonard@penzer.de>
2015-09-20 22:38:12 +0200

    fixed dhcp ip ranges

--- gen_config.py
+++ gen_config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 from string import Template
 from netaddr import *
-
+import argparse
 
 
 def genNetwork(segments, gw):
@@ -11,7 +11,8 @@ def genNetwork(segments, gw):
 	ip = IPNetwork("10.190.0.0/18")
 
 	for seg in segments:
-		inst = tmpl.substitute(gw="0%i"%(gw),seg=seg,ipv4=str(ip.network+1))
+		ipv4 = str(ip.network+gw)
+		inst = tmpl.substitute(gw="0%i"%(gw),seg=seg,ipv4=ipv4)
 		fp = open("etc/network/interfaces.d/ffs-gw%s%i"%(seg,gw), "wb")
 		fp.write(inst)
 		fp.close()
@@ -43,17 +44,22 @@ def genDhcp(segments, gw):
 
 	for seg in segments:
 		ipv4net = str(ip.network)
-		ipv4gw = str(ip.network+1)
-		ipv4start = str(ip.network+2)
-		ipv4end = str(ip.broadcast-1)
+		ipv4gw = str(ip.network+gw)
+		dhcp_ipnet = IPNetwork("%s/21"%(ip.network))
+		for  i in range(1,gw):
+			dhcp_ipnet = dhcp_ipnet.next()
+		
+		ipv4start = str(dhcp_ipnet.network+10)
+		ipv4end = str(dhcp_ipnet.broadcast-1)
+		
 		inst = tpl.substitute(ipv4start=ipv4start,ipv4end=ipv4end,ipv4net=ipv4net,ipv4gw=ipv4gw)
 		fp.write(inst)
-		ip = IPNetwork(str(ip.broadcast+1)+"/18")
+		ip = ip.next()
 	fp.close()
 
 
 segments = ["01","02","03","04"]
-gw=1
+gw=2
 genNetwork(segments,gw)
 genRadvd(segments,gw)
 genDhcp(segments,gw)
